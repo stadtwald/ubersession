52a885dd3e37ddecc656f920f38ffef8396e0480
William Demchick <william.demchick@gmail.com>
2025-05-10 17:53:22 +1200

    basic token issuance and verification

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,18 @@
 use axum::{Extension, Router, serve};
 use axum::extract::{Path, Request};
-use axum::http::header::{HeaderMap, HeaderValue, CACHE_CONTROL, HOST};
+use axum::http::header::{HeaderMap, HeaderValue, CACHE_CONTROL, COOKIE, HOST, SET_COOKIE};
 use axum::http::status::StatusCode;
 use axum::middleware::Next;
 use axum::response::{Html, IntoResponse};
 use axum::routing::{get, post};
-use ed25519_dalek::SigningKey;
+use chrono::Utc;
+use data_encoding::BASE64URL_NOPAD;
+use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
 use std::collections::HashSet;
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::TcpListener;
+use uuid::Uuid;
 
 #[derive(Clone, Debug)]
 pub struct Server {
@@ -92,6 +95,7 @@ impl Server {
 
             router = router.layer(Extension(Arc::new(settings)));
             router = router.layer(axum::middleware::from_fn(set_cache_control));
+            router = router.layer(axum::middleware::from_fn(token_cookie));
 
             Ok(Self {
                 listen: opts.listen,
@@ -154,5 +158,131 @@ async fn set_cache_control(request: Request, next: Next) -> impl IntoResponse {
     let mut headers = HeaderMap::new();
     headers.insert(CACHE_CONTROL, NO_CACHE);
     (headers, response)
+}
+
+#[derive(Clone, Debug)]
+pub struct SessionToken {
+    pub public_key: [u8; 32],
+    pub signature: Signature,
+    pub host: String, // secured
+    pub expires: u32, // secured
+    pub id: Uuid // secured
+}
+
+impl SessionToken {
+    fn new(signing_key: &SigningKey, ttl: u32, host: String) -> Self {
+        let current_timestamp = Utc::now().timestamp().try_into().unwrap_or(u32::MAX);
+        let expiry_timestamp = current_timestamp.saturating_add(ttl);
+        let mut session_token =
+            Self {
+                public_key: signing_key.verifying_key().as_bytes().clone(),
+                signature: Signature::from_bytes(&[0u8; 64]),
+                host: host,
+                expires: expiry_timestamp,
+                id: Uuid::new_v4()
+            };
+        session_token.signature = signing_key.sign(&session_token.signable_message());
+        session_token
+    }
+
+    fn signable_message(&self) -> Vec<u8> {
+        let mut buf = Vec::with_capacity(9 + 16 + 4 + self.host.len());
+        buf.extend("UBERSESS".as_bytes());
+        buf.push(0);
+        buf.extend(&self.expires.to_be_bytes());
+        buf.extend(self.id.as_bytes());
+        buf.extend(self.host.as_bytes());
+        buf
+    }
+
+    fn verify(&self, verifying_key: VerifyingKey) -> bool {
+        let signable_message = self.signable_message();
+        (&self.public_key == verifying_key.as_bytes()) && verifying_key.verify(&signable_message, &self.signature).is_ok()
+    }
+}
+
+impl From<SessionToken> for crate::wire::SessionToken {
+    fn from(token: SessionToken) -> crate::wire::SessionToken {
+        crate::wire::SessionToken {
+            public_key: BASE64URL_NOPAD.encode(&token.public_key),
+            signature: BASE64URL_NOPAD.encode(&token.signature.to_bytes()),
+            host: token.host,
+            expires: token.expires,
+            id: token.id
+        }
+    }
+}
+
+impl TryFrom<crate::wire::SessionToken> for SessionToken {
+    type Error = anyhow::Error;
+
+    fn try_from(wire: crate::wire::SessionToken) -> anyhow::Result<Self> {
+        let public_key: [u8; 32] = BASE64URL_NOPAD.decode(wire.public_key.as_bytes())?.as_slice().try_into()?;
+        let signature_bytes: [u8; 64] = BASE64URL_NOPAD.decode(wire.signature.as_bytes())?.as_slice().try_into()?;
+        let signature = Signature::from_bytes(&signature_bytes);
+        Ok(SessionToken {
+            public_key: public_key,
+            signature: signature,
+            host: wire.host,
+            expires: wire.expires,
+            id: wire.id
+        })
+    }
+}
+
+fn load_current_session_token(settings: Arc<Settings>, request_headers: HeaderMap) -> Option<SessionToken> {
+    let current_timestamp: u32 = Utc::now().timestamp().try_into().ok()?;
+    let cookie_header_value = request_headers.get(COOKIE)?.to_str().ok()?;
+    if cookie_header_value.len() > 4096 {
+        return None;
+    }
+    let raw_session_value =
+        {
+            let mut m_value = None;
+            for kv_pair in cookie_header_value.split("; ") {
+                if let Some((key, value)) = kv_pair.split_once('=') {
+                    if key == settings.cookie {
+                        m_value = Some(value);
+                        break;
+                    }
+                }
+            }
+            m_value?
+        };
+    let text_session_value = BASE64URL_NOPAD.decode(raw_session_value.as_bytes()).ok()?;
+    let session_token_descriptor: crate::wire::SessionToken = serde_json::from_slice(&text_session_value).ok()?;
+    let session_token: SessionToken = session_token_descriptor.try_into().ok()?;
+    if !session_token.verify(settings.signing_key.verifying_key()) {
+        return None;
+    }
+    let http_host = request_headers.get(HOST)?.to_str().ok()?;
+    if session_token.host != http_host {
+        return None;
+    }
+    if session_token.expires < current_timestamp {
+        return None;
+    }
+    Some(session_token)
+}
+
+async fn token_cookie(request_headers: HeaderMap, Extension(settings): Extension<Arc<Settings>>, mut request: Request, next: Next) -> impl IntoResponse {
+    let mut response_headers = HeaderMap::new();
+
+    let current_session_token =
+        if let Some(current_session_token) = load_current_session_token(settings.clone(), request_headers) {
+            current_session_token
+        } else {
+            let session_token = SessionToken::new(&settings.signing_key, settings.token_expiry, settings.authority.clone());
+            let session_token_descriptor: crate::wire::SessionToken = session_token.clone().into();
+            let encoded_session_token_descriptor = BASE64URL_NOPAD.encode(serde_json::to_string(&session_token_descriptor).unwrap().as_bytes());
+            response_headers.insert(SET_COOKIE, HeaderValue::from_str(&encoded_session_token_descriptor).unwrap());
+            session_token
+        };
+
+    request.extensions_mut().insert(current_session_token);
+
+    let response = next.run(request).await;
+
+    (response_headers, response)
 }
 
