afedf9a63154e44ab03eb8cf824daeeaf69b6eb3
William Demchick <william.demchick@gmail.com>
2025-05-29 16:18:45 +1200

    + ubersession_axum crate, containing utilities for axum apps consuming session tokens

---
+++ ubersession_axum/src/lib.rs
@@ -0,0 +1,137 @@
+use axum::Router;
+use axum::body::Body;
+use axum::http::StatusCode;
+use axum::http::header::{HeaderValue, LOCATION};
+use axum::http::request::Parts;
+use axum::response::Response;
+use axum::extract::{Extension, FromRequestParts};
+pub use ed25519_dalek::VerifyingKey;
+use std::future::ready;
+use std::sync::Arc;
+use uuid::Uuid;
+
+pub use ubersession_core::cookie::{CookieHeaderSource, CookieName};
+pub use ubersession_core::host_name::{HostName, HostNameSource};
+pub use ubersession_core::path_prefix::PathPrefix;
+pub use ubersession_core::session_token::{SessionToken, SessionTokenLoader};
+
+#[derive(Clone, Debug)]
+pub struct AxumSessionExtractionSettings {
+    path_prefix: PathPrefix,
+    cookie: CookieName,
+    verifying_key: VerifyingKey,
+    host_name: Option<HostName>
+}
+
+#[derive(Clone, Debug)]
+struct ExtractSettingsWrapper(Arc<AxumSessionExtractionSettings>);
+
+impl AxumSessionExtractionSettings {
+    pub fn new(verifying_key: VerifyingKey) -> Self {
+        Self {
+            path_prefix: PathPrefix::default(),
+            cookie: CookieName::escape_str("UBERSESSION"),
+            verifying_key: verifying_key,
+            host_name: None
+        }
+    }
+ 
+    pub fn with_path_prefix(mut self, path_prefix: PathPrefix) -> Self {
+        self.path_prefix = path_prefix;
+        self
+    }
+
+    pub fn with_cookie(mut self, cookie: CookieName) -> Self {
+        self.cookie = cookie;
+        self
+    }
+
+    pub fn with_host_name(mut self, host_name: HostName) -> Self {
+        self.host_name = Some(host_name);
+        self
+    }
+
+    pub fn setup_router(self, router: Router) -> Router {
+        router.layer(Extension(ExtractSettingsWrapper(Arc::new(self))))
+    }
+}
+
+fn extract_session_from_parts(settings: &AxumSessionExtractionSettings, parts: &Parts) -> Option<SessionToken> {
+    let http_host =
+        if let Some(host_name) = parts.headers.extract_host_name() {
+            host_name
+        } else {
+            return None;
+        };
+
+    let cookie_value =
+        if let Some(cookie_value) = parts.headers.extract_cookie(&settings.cookie) {
+            if let Ok(cookie_value) = cookie_value.unescape_str() {
+                cookie_value
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        };
+
+    let verification_host = settings.host_name.as_ref().unwrap_or(&http_host);
+
+    let session_token_loader =
+        SessionTokenLoader {
+            required_http_host: verification_host.clone(),
+            verifying_key: settings.verifying_key
+        };
+
+    session_token_loader.attempt_load(&cookie_value)
+}
+
+fn from_request_parts(parts: &Parts) -> Result<SessionToken, Response> {
+    let settings = {
+        let m_settings: Option<&ExtractSettingsWrapper> = parts.extensions.get();
+        if let Some(settings) = m_settings {
+            &settings.0
+        } else {
+            let mut response = Response::new(Body::empty());
+            *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+            return Err(response);
+        }
+    };
+
+    if let Some(session_token) = extract_session_from_parts(settings, parts) {
+        Ok(session_token.clone())
+    } else {
+        let mut response = Response::new(Body::empty());
+
+        if let Ok(location_header_value) = HeaderValue::try_from(format!("{}flow", settings.path_prefix)) {
+            *response.status_mut() = StatusCode::SEE_OTHER;
+            response.headers_mut().insert(LOCATION, location_header_value);
+        } else {
+            *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+        }
+
+        Err(response)
+    }
+}
+
+
+pub struct RequiredSessionToken(pub SessionToken);
+
+impl<S> FromRequestParts<S> for RequiredSessionToken {
+    type Rejection = Response;
+
+    fn from_request_parts(parts: &mut Parts, _state: &S) -> impl Future<Output = Result<Self, Response>> {
+        ready(from_request_parts(parts).map(|x| Self(x)))
+    }
+}
+
+pub struct RequiredSessionId(pub Uuid);
+
+impl<S> FromRequestParts<S> for RequiredSessionId {
+    type Rejection = Response;
+
+    fn from_request_parts(parts: &mut Parts, _state: &S) -> impl Future<Output = Result<Self, Response>> {
+         ready(from_request_parts(parts).map(|x| Self(x.id)))       
+    }
+}
+
